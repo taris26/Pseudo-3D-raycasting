f5e89c95d23369af8ae91af136abdbdd8e8fbb26
Luka Gojic <73824041+Gojicluka@users.noreply.github.com>
2022-10-17 16:52:17 +0200

    ima error oko crtanja

--- game.h
+++ game.h
@@ -12,8 +12,7 @@ class Update;
 class Player;
 class Game;
 #include "player.h"
-#include "update.h"
-#include "render.h"
+
 
 using namespace std;
 //sutra dan ce nam to biti posao
@@ -25,7 +24,7 @@ public:
 	void updateGame();
 	void renderGame();
 	void destroy();
-	Player player;
+	Player* player;
 	int sizex, sizey, width, height;
 	SDL_Window* winGrid;
 	SDL_Renderer* rendGrid;
@@ -33,9 +32,12 @@ public:
 	SDL_Renderer* rendPov;
 	vector<vector<bool>> wall;
 private:
-	Update update;
-	Render render;
+	Update* update;
+	Render* render;
 	
 	void pollEvents();
 
 };
+
+#include "update.h"
+#include "render.h"
