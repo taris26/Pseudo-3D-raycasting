b7b8edf8629cacc331ae77c61641d4da16cc35cf
taris26 <lakiristovic@live.com>
2022-10-17 21:12:27 +0200

    render fix

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,6 @@
 #include "Game.h"
 
 int main(int argc, char* argv[]) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		printf("error initializing SDL: %s\n", SDL_GetError());
-	}
 	Game game;
 	game.init();
 	while (!game.close) {
