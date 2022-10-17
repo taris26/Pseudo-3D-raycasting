dc9f4a5dca4b5d4b60f237b30ba15d24807d332d
taris26 <lakiristovic@live.com>
2022-10-17 21:12:27 +0200

    render fix

--- Player.h
+++ Player.h
@@ -4,7 +4,6 @@ class Player
 {
 public:
 	int x, y, xpos, ypos, playerWidth = 10, playerHeight = 10, speed = 10;
-	double  langle, rangle, cangle, fov;
+	double  langle, rangle, cangle = 3 * M_PI / 2, fov = M_PI / 3;
 	void move(double angle, Game& game);
 };
-
