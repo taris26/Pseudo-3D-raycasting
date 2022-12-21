3d0046448a3149292fd309ee0f92107b337411e4
taris26 <lakiristovic@live.com>
2022-12-21 16:54:21 +0100

    wall overlap bug fix

--- Player.h
+++ Player.h
@@ -5,7 +5,7 @@ class Player
 public:
 	int x = 0, y = 0;
 	const int playerWidth = 10, playerHeight = 10, speed = 5;
-	double  langle = 0, rangle = 0, cangle = 4.245575, xpos = 500, ypos = 700;
+	double  langle = 0, rangle = 0, cangle = 9.64513, xpos = 602.488, ypos = 82.8019;
 	const double  fov = M_PI / 3;
 	void move(double angle, Game& game);
 };
--- Render.h
+++ Render.h
@@ -11,7 +11,7 @@ private:
 	double ang(double x1, double y1, double x2, double y2);
 	pair<int, int> presek(double angle, Player& player, int i, int j, Game& game);
 	void triangle(Game& game, float x1, float y1, float x2, float y2, float x3, float y3, SDL_Texture* tekstura);
-	void trapistSir(SDL_Renderer* rend2, Player& player, int x2, int y2, int x3, int y3);
+	void trapistSir(SDL_Renderer* rend2, Player& player, int x2, int y2, int x3, int y3, double ang2, double ang3);
 	int sign(int x);
 };
 
